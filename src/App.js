6ecb440a04e27852e1dd1c96e706ef37f4e71d30
xingchengyusi <zsxxxzks1314@gmail.com>
2019-11-25 10:59:37 -0800

    fix router

--- src/App.js
+++ src/App.js
@@ -99,26 +99,26 @@ class Trails_map extends React.Component {
 }
 
 class Trails extends  React.Component {
-  filterTrails(trails) {
+  filterTrails() {
     let filter = {
       weather: 1,
       condition: 1,
     };
 
     if (filter.weather === 1)
-      this.filterByWeather(trails);
+      this.filterByWeather();
     if (filter.condition === 1)
-      this.filterByCondition(trails);
+      this.filterByCondition();
   }
 
-  filterByWeather(trails) {
+  filterByWeather() {
   }
 
-  filterByCondition(trails) {
+  filterByCondition() {
     let time = new Date();
     // filter condition in all trails
-    for (let i = 0; i < trails.length; i++) {
-      let t = trails[i];
+    for (let i = 0; i < this.trails.length; i++) {
+      let t = this.trails[i];
       // available param: 0: danger 1: safe 2: don't know
       t.available = 1;
 
@@ -134,16 +134,128 @@ class Trails extends  React.Component {
     }
   }
 
+  static getTrails() {
+    const exTrails = {
+      "trails": [
+        {
+          "id": 7000130,
+          "name": "Bear Peak Out and Back",
+          "type": "Featured Hike",
+          "summary": "A must-do hike for Boulder locals and visitors alike!",
+          "difficulty": "blueBlack",
+          "stars": 4.6,
+          "starVotes": 109,
+          "location": "Boulder, Colorado",
+          "url": "https:\/\/www.hikingproject.com\/trail\/7000130\/bear-peak-out-and-back",
+          "imgSqSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7005382_sqsmall_1554312030.jpg",
+          "imgSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7005382_small_1554312030.jpg",
+          "imgSmallMed": "https:\/\/cdn-files.apstatic.com\/hike\/7005382_smallMed_1554312030.jpg",
+          "imgMedium": "https:\/\/cdn-files.apstatic.com\/hike\/7005382_medium_1554312030.jpg",
+          "length": 5.7,
+          "ascent": 2541,
+          "descent": -2540,
+          "high": 8342,
+          "low": 6103,
+          "longitude": -105.2755,
+          "latitude": 39.9787,
+          "conditionStatus": "Unknown",
+          "conditionDetails": null,
+          "conditionDate": "1970-01-01 00:00:00"
+        },
+        {
+          "id": 7001019,
+          "name": "Betasso Preserve",
+          "type": "Featured Hike",
+          "summary": "This hike is easily accessible from Boulder and offers amazing singletrack with beautiful views.",
+          "difficulty": "blue",
+          "stars": 4.1,
+          "starVotes": 60,
+          "location": "Boulder, Colorado",
+          "url": "https:\/\/www.hikingproject.com\/trail\/7001019\/betasso-preserve",
+          "imgSqSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7029200_sqsmall_1554920151.jpg",
+          "imgSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7029200_small_1554920151.jpg",
+          "imgSmallMed": "https:\/\/cdn-files.apstatic.com\/hike\/7029200_smallMed_1554920151.jpg",
+          "imgMedium": "https:\/\/cdn-files.apstatic.com\/hike\/7029200_medium_1554920151.jpg",
+          "length": 6.7,
+          "ascent": 776,
+          "descent": -778,
+          "high": 6575,
+          "low": 6178,
+          "longitude": -105.3446,
+          "latitude": 40.0164,
+          "conditionStatus": "All Clear",
+          "conditionDetails": "Mostly Dry, Some Mud - 90%+ clear and dry with few muddy patches",
+          "conditionDate": "2019-11-16 13:57:20"
+        },
+        {
+          "id": 7017569,
+          "name": "Marshall Mesa to Spring Brook Loop",
+          "type": "Featured Hike",
+          "summary": "Some of the best trails that Boulder has to offer with a variety of options that never get old.",
+          "difficulty": "blue",
+          "stars": 4.3,
+          "starVotes": 26,
+          "location": "Superior, Colorado",
+          "url": "https:\/\/www.hikingproject.com\/trail\/7017569\/marshall-mesa-to-spring-brook-loop",
+          "imgSqSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7002458_sqsmall_1554226116.jpg",
+          "imgSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7002458_small_1554226116.jpg",
+          "imgSmallMed": "https:\/\/cdn-files.apstatic.com\/hike\/7002458_smallMed_1554226116.jpg",
+          "imgMedium": "https:\/\/cdn-files.apstatic.com\/hike\/7002458_medium_1554226116.jpg",
+          "length": 11.1,
+          "ascent": 893,
+          "descent": -893,
+          "high": 6236,
+          "low": 5567,
+          "longitude": -105.2313,
+          "latitude": 39.9527,
+          "conditionStatus": "All Clear",
+          "conditionDetails": "Muddy, Snowy - Super fun but challenging in these conditions.",
+          "conditionDate": "2019-11-04 23:41:09"
+        },
+        {
+          "id": 7005887,
+          "name": "Sugarloaf Mountain",
+          "type": "Featured Hike",
+          "summary": "The best bang-for-your-buck view trail in Boulder County.",
+          "difficulty": "greenBlue",
+          "stars": 4.4,
+          "starVotes": 19,
+          "location": "Boulder, Colorado",
+          "url": "https:\/\/www.hikingproject.com\/trail\/7005887\/sugarloaf-mountain",
+          "imgSqSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7031490_sqsmall_1554931128.jpg",
+          "imgSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7031490_small_1554931128.jpg",
+          "imgSmallMed": "https:\/\/cdn-files.apstatic.com\/hike\/7031490_smallMed_1554931128.jpg",
+          "imgMedium": "https:\/\/cdn-files.apstatic.com\/hike\/7031490_medium_1554931128.jpg",
+          "length": 1.4,
+          "ascent": 432,
+          "descent": -432,
+          "high": 8892,
+          "low": 8460,
+          "longitude": -105.4251,
+          "latitude": 40.0255,
+          "conditionStatus": "Minor Issues",
+          "conditionDetails": "Dry",
+          "conditionDate": "2019-09-11 21:25:47"
+        }
+      ],
+      "success": 1
+    };
+
+    if (exTrails.success === 0)
+      alert('Get Trails from Hiking Project failed.');
+
+    return exTrails.trails;
+  }
+
   render() {
-    let trails = getTrails();
-    this.filterTrails(trails);
+    this.trails = Trails.getTrails();
+    this.filterTrails();
 
     return (
       <div className="trails">
-        {/*<Trails_find getTrails={getTrails} />*/}
         <div className='trails-title'>Hot Trails</div>
-        <Trails_list trails={trails} />
-        <Trails_map trails={trails} id='trails-map' options={{center: { lat: 45.5051, lng: -122.6750 }, zoom: 13, mapTypeId: 'terrain'}} />
+        <Trails_list trails={this.trails} />
+        <Trails_map trails={this.trails} id='trails-map' options={{center: { lat: 45.5051, lng: -122.6750 }, zoom: 13, mapTypeId: 'terrain'}} />
       </div>
     );
   }
@@ -152,124 +264,11 @@ class Trails extends  React.Component {
 function App() {
   return (
     <div>
-      <Welcome title={(<Trails_find getTrails={getTrails} />)} />
+      <Welcome title={(<Trails_find getTrails={Trails.getTrails} />)} />
       <Trails />
       <Footer />
     </div>
   );
-}
-
-function getTrails() {
-  const exTrails = {
-    "trails": [
-      {
-        "id": 7000130,
-        "name": "Bear Peak Out and Back",
-        "type": "Featured Hike",
-        "summary": "A must-do hike for Boulder locals and visitors alike!",
-        "difficulty": "blueBlack",
-        "stars": 4.6,
-        "starVotes": 109,
-        "location": "Boulder, Colorado",
-        "url": "https:\/\/www.hikingproject.com\/trail\/7000130\/bear-peak-out-and-back",
-        "imgSqSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7005382_sqsmall_1554312030.jpg",
-        "imgSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7005382_small_1554312030.jpg",
-        "imgSmallMed": "https:\/\/cdn-files.apstatic.com\/hike\/7005382_smallMed_1554312030.jpg",
-        "imgMedium": "https:\/\/cdn-files.apstatic.com\/hike\/7005382_medium_1554312030.jpg",
-        "length": 5.7,
-        "ascent": 2541,
-        "descent": -2540,
-        "high": 8342,
-        "low": 6103,
-        "longitude": -105.2755,
-        "latitude": 39.9787,
-        "conditionStatus": "Unknown",
-        "conditionDetails": null,
-        "conditionDate": "1970-01-01 00:00:00"
-      },
-      {
-        "id": 7001019,
-        "name": "Betasso Preserve",
-        "type": "Featured Hike",
-        "summary": "This hike is easily accessible from Boulder and offers amazing singletrack with beautiful views.",
-        "difficulty": "blue",
-        "stars": 4.1,
-        "starVotes": 60,
-        "location": "Boulder, Colorado",
-        "url": "https:\/\/www.hikingproject.com\/trail\/7001019\/betasso-preserve",
-        "imgSqSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7029200_sqsmall_1554920151.jpg",
-        "imgSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7029200_small_1554920151.jpg",
-        "imgSmallMed": "https:\/\/cdn-files.apstatic.com\/hike\/7029200_smallMed_1554920151.jpg",
-        "imgMedium": "https:\/\/cdn-files.apstatic.com\/hike\/7029200_medium_1554920151.jpg",
-        "length": 6.7,
-        "ascent": 776,
-        "descent": -778,
-        "high": 6575,
-        "low": 6178,
-        "longitude": -105.3446,
-        "latitude": 40.0164,
-        "conditionStatus": "All Clear",
-        "conditionDetails": "Mostly Dry, Some Mud - 90%+ clear and dry with few muddy patches",
-        "conditionDate": "2019-11-16 13:57:20"
-      },
-      {
-        "id": 7017569,
-        "name": "Marshall Mesa to Spring Brook Loop",
-        "type": "Featured Hike",
-        "summary": "Some of the best trails that Boulder has to offer with a variety of options that never get old.",
-        "difficulty": "blue",
-        "stars": 4.3,
-        "starVotes": 26,
-        "location": "Superior, Colorado",
-        "url": "https:\/\/www.hikingproject.com\/trail\/7017569\/marshall-mesa-to-spring-brook-loop",
-        "imgSqSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7002458_sqsmall_1554226116.jpg",
-        "imgSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7002458_small_1554226116.jpg",
-        "imgSmallMed": "https:\/\/cdn-files.apstatic.com\/hike\/7002458_smallMed_1554226116.jpg",
-        "imgMedium": "https:\/\/cdn-files.apstatic.com\/hike\/7002458_medium_1554226116.jpg",
-        "length": 11.1,
-        "ascent": 893,
-        "descent": -893,
-        "high": 6236,
-        "low": 5567,
-        "longitude": -105.2313,
-        "latitude": 39.9527,
-        "conditionStatus": "All Clear",
-        "conditionDetails": "Muddy, Snowy - Super fun but challenging in these conditions.",
-        "conditionDate": "2019-11-04 23:41:09"
-      },
-      {
-        "id": 7005887,
-        "name": "Sugarloaf Mountain",
-        "type": "Featured Hike",
-        "summary": "The best bang-for-your-buck view trail in Boulder County.",
-        "difficulty": "greenBlue",
-        "stars": 4.4,
-        "starVotes": 19,
-        "location": "Boulder, Colorado",
-        "url": "https:\/\/www.hikingproject.com\/trail\/7005887\/sugarloaf-mountain",
-        "imgSqSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7031490_sqsmall_1554931128.jpg",
-        "imgSmall": "https:\/\/cdn-files.apstatic.com\/hike\/7031490_small_1554931128.jpg",
-        "imgSmallMed": "https:\/\/cdn-files.apstatic.com\/hike\/7031490_smallMed_1554931128.jpg",
-        "imgMedium": "https:\/\/cdn-files.apstatic.com\/hike\/7031490_medium_1554931128.jpg",
-        "length": 1.4,
-        "ascent": 432,
-        "descent": -432,
-        "high": 8892,
-        "low": 8460,
-        "longitude": -105.4251,
-        "latitude": 40.0255,
-        "conditionStatus": "Minor Issues",
-        "conditionDetails": "Dry",
-        "conditionDate": "2019-09-11 21:25:47"
-      }
-    ],
-    "success": 1
-  };
-
-  if (exTrails.success === 0)
-    alert('Get Trails from Hiking Project failed.');
-
-  return exTrails.trails;
 }
 
 export default App;
