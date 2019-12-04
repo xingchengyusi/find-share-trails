3cfa49e15c17d2237689f74db8617bf8c98a7d87
xingchengyusi <zsxxxzks1314@gmail.com>
2019-12-04 06:00:05 -0800

    update about

--- src/About/AboutApp.js
+++ src/About/AboutApp.js
@@ -6,6 +6,11 @@ export default class AboutApp extends React.Component {
       <div className='intro'>
         <div className='intro-title'>About This App</div>
         <div className='intro-normal'>The is a Web Application that create by React.</div>
+        <div className='intro-third'>React</div>
+        <div className='intro-third'>Google Script</div>
+        <div className='intro-third'>Google Sheet</div>
+        <div className='intro-third'>Hiking Project</div>
+        <div className='intro-third'>Google Map</div>
       </div>
     );
   }
